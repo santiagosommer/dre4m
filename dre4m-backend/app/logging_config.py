975a408440f9d2e728a057da157ca9235a15a014
santiagosommer <s.sommerg@gmail.com>
2025-03-02 17:28:45 -0300

    added level validation and console handler for console output

--- dre4m-backend/app/logging_config.py
+++ dre4m-backend/app/logging_config.py
@@ -9,9 +9,16 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
+
 # Get the logging level from an environment variable, DEBUG as default
 log_level = os.getenv('LOG_LEVEL', 'DEBUG').upper()
 
+# Validate the logging level
+valid_log_levels = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
+if log_level not in valid_log_levels:
+    log_level = 'DEBUG'
+    logging.warning(
+        "Invalid LOG_LEVEL environment variable. Defaulting to DEBUG.")
 
 # Configure logging
 logging.basicConfig(
@@ -21,7 +28,15 @@ logging.basicConfig(
     level=getattr(logging, log_level, logging.DEBUG)
 )
 
-logger = logging.getLogger(__name__)
+# Add a console handler for showing logs in the console
+console_handler = logging.StreamHandler()
+console_handler.setLevel(getattr(logging, log_level, logging.DEBUG))
+console_handler.setFormatter(logging.Formatter(
+    '%(asctime)s - %(levelname)s - %(message)s'))
+logging.getLogger().addHandler(console_handler)
+
+# Create a logger
+logger = logging.getLogger('dre4m-backend')
 
 if __name__ == '__main__':
     pass

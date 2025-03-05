b3749aa3ff879383126c0ffafe11d28542278e3f
santiagosommer <s.sommerg@gmail.com>
2025-03-05 12:11:59 -0300

    Completed init_database with db user and user table creation, also completed user CRUD without user_update. Updated .gitignore

--- dre4m-backend/app/db/init_db.py
+++
@@ -1,7 +0,0 @@
-# From imports
-from .utils.utils_db import create_db_user
-from .utils.utils_db import create_table
-
-if __name__ == "__main__":
-    create_db_user()
-    create_table()
--- dre4m-backend/app/db/utils/utils_db.py
+++ dre4m-backend/app/db/utils/utils_db.py
@@ -1,33 +1,59 @@
+# Imports
+import os
+
 # From imports
-from ...exceptions import InitDatabaseError
+from dotenv import load_dotenv
+from .init_queries import CREATE_USERS_TABLE_QUERY
 from ..connection import SessionLocal
-from sqlalchemy.exc import SQLAlchemyError
-from .sql_queries import CREATE_USERS_TABLE_QUERY
-from .sql_queries import CREATE_DB_USERS
+from ...exceptions import InitDatabaseError
 from ...logging_config import logger
+
+# SQKAlchemy imports
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.sql import text
+
+# Remove after developing the app
+load_dotenv()
+
+DB_USER_QUERY = os.getenv(
+    "CREATE_DB_USERS_QUERY"
+)
 
 
 def create_db_user():
+    """
+    Creates the database user 'dream_admin' if it does not exist.
+    It logs the result of the operation.
+    """
     try:
         # Create the new database user
         db = SessionLocal()
-        db.execute(CREATE_DB_USERS)
+
+        if DB_USER_QUERY is None:
+            raise InitDatabaseError(
+                "DB_USER_QUERY environment variable is not set")
+
+        db.execute(text(DB_USER_QUERY))
         db.commit()
         db.close()
-        logger.info("Database user 'dream_admin' created successfully.")
+        logger.info("Database admin user created successfully.")
     except SQLAlchemyError:
         logger.error("Error creating database user 'dream_admin'")
         raise InitDatabaseError("Error creating database user")
 
 
 def create_table():
+    """
+    Creates the table 'users' if it does not exist.
+    It logs the result of the operation.
+    """
     try:
         # Create the new database user
         db = SessionLocal()
         db.execute(CREATE_USERS_TABLE_QUERY)
         db.commit()
         db.close()
-        logger.info("Table 'users' created successfully)")
+        logger.info("Users table created successfully)")
     except SQLAlchemyError:
         logger.error("Error creating table 'users'")
         raise InitDatabaseError("Error creating database user")

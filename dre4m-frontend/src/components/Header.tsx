8fddf6fff7a9e46c8d2039e5b11734d67ee668f1
santiagosommer <s.sommerg@gmail.com>
2025-03-05 16:42:25 -0300

    Initial frontend commit

---
+++ dre4m-frontend/src/components/Header.tsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+    return (
+        <div>
+            <ul>
+                <li> hey</li>
+                <li> hey</li>
+                <li> hey</li>
+            </ul>
+        </div>
+    )
+}

6aeedcc4ad77a37f562d38b99e2020551b8293fa
santiagosommer <s.sommerg@gmail.com>
2025-03-09 12:13:53 -0300

    Added CORS config to enable connection between backend and frontend, corrected user model

--- dre4m-backend/app/api/main.py
+++ dre4m-backend/app/api/main.py
@@ -1,12 +1,27 @@
 # From imports
-from typing import Union
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+from .models.users_api_models import User
 
 # Loads environment variables from .env file
 load_dotenv()
 
 app = FastAPI()
+
+# Configuración de CORS
+origins = [
+    "http://localhost:5173",  # Reemplaza con el origen de tu frontend
+    "http://127.0.0.1:8000",  # Reemplaza con el origen de tu frontend
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -14,9 +29,7 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(
-    item_id: int,
-    q: Union[str, None] = None
-) -> dict[str, Union[int, Union[str, None]]]:
-    return {"item_id": item_id, "q": q}
+@app.post("/users/")
+def create_user(user: User):
+    print(user.email, user.password)
+    return {"user": user}
--- dre4m-backend/app/api/models/users_api_models.py
+++ dre4m-backend/app/api/models/users_api_models.py
@@ -1,42 +1,16 @@
+from pydantic import BaseModel, EmailStr
+
 # Object definition for users
-class User:
-    def __init__(self, email: str, password: str):
-        self.email = email
-        self.password = password
+
+
+class User(BaseModel):
+    email: EmailStr
+    password: str
 
 
 class Admin(User):
-    def __init__(self, email: str, password: str, role: str):
-        super().__init__(email, password)
-        self.role = role
+    role: str
 
 
 class Customer(User):
-    def __init__(self, email: str, password: str):
-        super().__init__(email, password)
-        self.email = email
-
-# Temporal function definitions
-
-
-def crear_usuario():
-    print("Función para crear usuario...")
-    email = input("Nombre de usuario: ")
-    password = input("Contraseña: ")
-    email = input("Correo electrónico: ")
-    user = Customer(email, password)
-    print(f"Usuario {user.email} creado exitosamente.")
-    return user
-
-
-def eliminar_usuario():
-    print("Función para eliminar usuario...")
-
-
-def listar_usuarios():
-    print("Función para listar usuarios...")
-
-
-def salir():
-    print("Saliendo del programa...")
-    exit()
+    pass

f6a9fed83bbb69679bff841457ee37d0a0fd2013
santiagosommer <s.sommerg@gmail.com>
2025-03-09 11:57:35 -0300

    Added a reusable FormComponent with input fields and a Navbar

---
+++ dre4m-frontend/src/components/InputField.tsx
@@ -0,0 +1,14 @@
+export const InputField = () => {
+    return (
+        <>
+            <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                required
+            />
+        </>
+    )
+}
+
+export default InputField

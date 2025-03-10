fa425198717a72ddeaca1fd20304be1a675d4874
santiagosommer <s.sommerg@gmail.com>
2025-03-10 14:10:31 -0300

    Create user form component, added to auth page

--- dre4m-frontend/src/pages/Auth.tsx
+++ dre4m-frontend/src/pages/Auth.tsx
@@ -1,9 +1,12 @@
 import { LoginForm } from "../components/LoginForm"
+import { CreateUserForm } from "../components/CreateUserForm"
+
 export const Auth = () => {
   return (
     <>
-      <h1>Create User</h1>
+      <h1>Login or Create User</h1>
       <LoginForm />
+      <CreateUserForm />
     </>
   )
 }

5c848c36b0b8ee8813732f0b66d6018914872298
santiagosommer <s.sommerg@gmail.com>
2025-03-12 15:42:12 -0300

    init_db now creates a database, a database user and every table in a single line

--- dre4m-backend/app/db/utils/init_db.py
+++ dre4m-backend/app/db/utils/init_db.py
@@ -1,13 +1,14 @@
+# From imports
+from app.db.utils.utils_db import (
+    create_db_user,
+    create_tables,
+    create_db
+)
 """
 This script is used to create the database user and the table in the database.
 """
 
-# From imports
-from .utils_db import create_db_user
-from .utils_db import create_table
-
-
 if __name__ == "__main__":
-    # Missing db creation
+    create_db()
     create_db_user()
-    create_table()
+    create_tables()
--- dre4m-backend/app/db/utils/utils_db.py
+++ dre4m-backend/app/db/utils/utils_db.py
@@ -1,59 +1,114 @@
 # Imports
 import os
+import psycopg2
 
 # From imports
+from sqlalchemy import create_engine
 from dotenv import load_dotenv
-from .init_queries import CREATE_USERS_TABLE_QUERY
-from ..connection import SessionLocal
-from ...exceptions import InitDatabaseError
-from ...logging_config import logger
+from app.exceptions import MissingEnvironmentVariableError
+from app.logging_config import logger
+from app.db.models.users_models import Base
 
-# SQLAlchemy imports
-from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.sql import text
+"""
+This module contains utility functions for creating the database and tables.
+
+Important: If changing the way to get the data from environment variables,
+parametrize the queries in the functions to avoid SQL injection attacks.
+"""
 
 # Remove after developing the app
 load_dotenv()
 
-DB_USER_QUERY = os.getenv(
-    "CREATE_DB_USERS_QUERY"
-)
+# Missing password in the connection string
+DEFAULT_DB_USER = os.getenv("DEFAULT_DB_USER")
+DB_NEW_USER = os.getenv("DB_USER")
+DB_NEW_USER_PASSWORD = os.getenv("DB_USER_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+if not all([DEFAULT_DB_USER, DB_NEW_USER,
+            DB_NEW_USER_PASSWORD, DB_HOST,
+            DB_PORT, DB_NAME]):
+    raise MissingEnvironmentVariableError("Missing environment variables")
+
+
+def create_db():
+    """
+    Creates the database if it does not exist.
+    """
+    try:
+        conn = psycopg2.connect(
+            dbname="postgres",
+            user=DEFAULT_DB_USER,
+            host=DB_HOST,
+            port=DB_PORT
+        )
+        conn.autocommit = True
+        cur = conn.cursor()
+
+        # Check if the database exists
+        cur.execute(
+            "SELECT 1 FROM pg_database WHERE datname = %s;", (DB_NAME,)
+        )
+        exists = cur.fetchone()
+
+        if not exists:
+            cur.execute(
+                f"CREATE DATABASE {DB_NAME};"
+            )
+            logger.info(f"Database {DB_NAME} created.")
+        else:
+            logger.info(f"Database {DB_NAME} already exists.")
+
+        cur.close()
+        conn.close()
+
+    except Exception as e:
+        logger.error(f"Error creating database: {e}")
 
 
 def create_db_user():
     """
-    Creates the database user 'dream_admin' if it does not exist.
-    It logs the result of the operation.
+    Creates a database user if it does not exist.
+    Ensure that create database function is called before this function.
     """
-    try:
-        # Create the new database user
-        db = SessionLocal()
+    conn = psycopg2.connect(
+        dbname=DB_NAME,
+        user=DEFAULT_DB_USER,
+        host=DB_HOST,
+        port=DB_PORT
+    )
+    conn.autocommit = True
+    cur = conn.cursor()
 
-        if DB_USER_QUERY is None:
-            raise InitDatabaseError(
-                "DB_USER_QUERY environment variable is not set")
+    # Check if the user already exists
+    cur.execute(
+        f"SELECT 1 FROM pg_roles WHERE rolname='{DB_NEW_USER}';"
+    )
+    exists = cur.fetchone()
 
-        db.execute(text(DB_USER_QUERY))
-        db.commit()
-        db.close()
-        logger.info("Database admin user created successfully.")
-    except SQLAlchemyError:
-        logger.error("Error creating database user 'dream_admin'")
-        raise InitDatabaseError("Error creating database user")
+    if not exists:
+        cur.execute(
+            f"CREATE USER {DB_NEW_USER} WITH "
+            f"PASSWORD '{DB_NEW_USER_PASSWORD}' "
+            f"CREATEDB CREATEROLE REPLICATION BYPASSRLS LOGIN;"
+        )
+        logger.info(f"User {DB_NEW_USER} created.")
+    else:
+        logger.info(f"User {DB_NEW_USER} already exists.")
+
+    cur.close()
+    conn.close()
 
 
-def create_table():
+def create_tables():
     """
-    Creates the table 'users' if it does not exist.
-    It logs the result of the operation.
+    Creates the tables defined in the SQLAlchemy models.
     """
-    try:
-        # Create the new database user
-        db = SessionLocal()
-        db.execute(CREATE_USERS_TABLE_QUERY)
-        db.commit()
-        db.close()
-        logger.info("Users table created successfully)")
-    except SQLAlchemyError:
-        logger.error("Error creating table 'users'")
-        raise InitDatabaseError("Error creating database user")
+    DATABASE_URL = (
+        f"postgresql://{DEFAULT_DB_USER}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+    engine = create_engine(DATABASE_URL, echo=False)
+    Base.metadata.create_all(bind=engine)
+    print(f'Created tables: {list(Base.metadata.tables.keys())}')

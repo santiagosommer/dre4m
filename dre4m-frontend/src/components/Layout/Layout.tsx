25f4caccc3e51255e67a2061ee80f18fc39cbdbc
santiagosommer <s.sommerg@gmail.com>
2025-05-26 10:39:44 -0300

    sidecart working in layout with backdrop

--- dre4m-frontend/src/components/Layout/Layout.tsx
+++ dre4m-frontend/src/components/Layout/Layout.tsx
@@ -2,12 +2,19 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import './Layout.css'
 import Marquee from "../Marquee/Marquee";
+import SideCart from "../SideCart/SideCart";
+import { useState } from "react";
 
 function Layout() {
+    const [showCart, setShowCart] = useState(false);
+
     return (
         <>
-            <Navbar />
+            <Navbar onCartClick={() => setShowCart(true)} />
             <Marquee />
+            {/* Backdrop */}
+            {showCart && <div className="cart-backdrop" onClick={() => setShowCart(false)} />}
+            <SideCart open={showCart} onClose={() => setShowCart(false)} />
             <main className="content">
                 <Outlet />
             </main>

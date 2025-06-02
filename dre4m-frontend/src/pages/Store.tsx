6a39d10d453bd7bb41562dc5a6bda2a380858298
santiagosommer <s.sommerg@gmail.com>
2025-06-02 10:21:19 -0300

    updated background src

--- dre4m-frontend/src/pages/Store.tsx
+++ dre4m-frontend/src/pages/Store.tsx
@@ -1,9 +1,9 @@
 import './Store.css'
-import background from '../assets/shop_background.png'
 import ProductCard from '../components/ProductCard/ProductCard'
 import { Link } from 'react-router-dom'
 import { useProducts } from "../context/ProductsContext"
 
+const background = 'https://zkdwvxlhnamlrrtdgakp.supabase.co/storage/v1/object/sign/dre4m/shop_background.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InN0b3JhZ2UtdXJsLXNpZ25pbmcta2V5XzlkNGU3MzllLTdkMjktNDE0ZS1hZmQwLTc2MDVhN2M0NmY4ZSJ9.eyJ1cmwiOiJkcmU0bS9zaG9wX2JhY2tncm91bmQucG5nIiwiaWF0IjoxNzQ4MzU0ODQ3LCJleHAiOjIwNjM3MTQ4NDd9.vWb2T1a-z1Ght0VZUkC_Kq_5Oj47os0TiOWjKgPWdRY';
 
 export const Store = () => {
     const { products } = useProducts()
@@ -25,7 +25,7 @@ export const Store = () => {
                                 to={`/product/${products.id}`}
                                 style={{ textDecoration: "none", color: "inherit" }}
                             >
-                                <ProductCard name={products.name} price={products.price} />
+                                <ProductCard name={products.name} price={products.price} image={products.img} />
                             </Link>
                         ))}
                     </div>
